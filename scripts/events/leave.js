a111d7b60229506b0bc6253cd02222df6eac4b4a
Nobita <spammer993@outlook.com>
2024-10-09 08:17:56 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -13,14 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "Bye Bye Idol _ আহারে চলে গেলো..🫵🤣" : "Kicked by Administrator";
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "কিরে তুই কোথায় পালাস,আমি রানা বট 🤖 থাকতে পালাতে পারবি না...🫵🙈" : "Kicked by Administrator";
 	const path = join(__dirname, "nayan", "leaveGif");
 	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "তুই {name} কোথায় পালাস আমি রানা বট থাকতে পালাতে পারবি না 🫵🤣 .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = "~Bye Bye Idol {name} _Reason : আহারে চলে গেলো..🫵🐸 .\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
