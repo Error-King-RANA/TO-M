0526a672166ecbaa74934ae26ec704be65a3374c
Nobita <spammer993@outlook.com>
2024-10-18 17:26:18 +0600

    Update ball.js

--- scripts/commands/ball.js
+++ scripts/commands/ball.js
@@ -1,48 +1,75 @@
 module.exports.config = {
-    name: "ball", 
-    version: "1.0.0", 
-    permission: 0,
-    credits: "SK-SIDDIK-KHAN",
-    description: "",
-    prefix: true,
-    category: "love", 
-    usages: "user", 
-    cooldowns: 5,
-    dependencies: {
+  name: "ball", 
+  version: "1.0.0", 
+  permission: 0,
+  credits: "SK-SIDDIK-KHAN",
+  description: "",
+  prefix: true,
+  category: "loove", 
+  usages: "user", 
+  cooldowns: 5,
+  dependencies: {
         "axios": "",
         "fs-extra": "",
         "path": "",
         "jimp": ""
     }
 };
-
-module.exports.onStart = async function ({ message, event, args }) {
+ 
+module.exports.onLoad = async() => {
+    const { resolve } = global.nodemodule["path"];
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { downloadFile } = global.utils;
+    const dirMaterial = __dirname + `/cache/canvas/`;
+    const path = resolve(__dirname, 'cache/canvas', 'arr2.png');
+    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
+    if (!existsSync(path)) await downloadFile("https://i.ibb.co/6Jz7yvX/image.jpg", path); 
+}
+ 
+async function makeImage({ one, two }) {
+    const fs = global.nodemodule["fs-extra"];
+    const path = global.nodemodule["path"];
+    const axios = global.nodemodule["axios"]; 
+    const jimp = global.nodemodule["jimp"];
+    const __root = path.resolve(__dirname, "cache", "canvas");
+ 
+    let batgiam_img = await jimp.read(__root + "/arr2.png");
+    let pathImg = __root + `/batman${one}_${two}.png`;
+    let avatarOne = __root + `/avt_${one}.png`;
+    let avatarTwo = __root + `/avt_${two}.png`;
+    
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+    
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+    
+    let circleOne = await jimp.read(await circle(avatarOne));
+    let circleTwo = await jimp.read(await circle(avatarTwo));
+    batgiam_img.composite(circleOne.resize(170, 170), 200, 320).composite(circleTwo.resize(170, 170), 610, 70);
+    
+    let raw = await batgiam_img.getBufferAsync("image/png");
+    
+    fs.writeFileSync(pathImg, raw);
+    fs.unlinkSync(avatarOne);
+    fs.unlinkSync(avatarTwo);
+    
+    return pathImg;
+}
+async function circle(image) {
+    const jimp = require("jimp");
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+}
+ 
+module.exports.run = async function ({ event, api, args }) {    
+    const fs = global.nodemodule["fs-extra"];
+    const { threadID, messageID, senderID } = event;
     const mention = Object.keys(event.mentions);
-    if (mention.length == 0) {
-        return message.reply("Please mention someone to kick the ball.");
-    } else {
-        const one = event.senderID, two = mention[0];
-        bal(one, two).then(ptth => { 
-            message.reply({ 
-                body: "Here is your ball shot! 😹", 
-                attachment: fs.createReadStream(ptth) 
-            });
-        });
+    if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
+    else {
+        const one = senderID, two = mention[0];
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "ball", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
-};
-
-async function bal(one, two) {
-    let avone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
-    avone.circle();
-    let avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
-    avtwo.circle();
-    let pth = "ball.png";
-    let img = await jimp.read("https://i.ibb.co/6Jz7yvX/image.jpg");
-
-    img.resize(1080, 1320)
-       .composite(avone.resize(170, 170), 200, 320)
-       .composite(avtwo.resize(170, 170), 610, 70);
-
-    await img.writeAsync(pth);
-    return pth;
-                }
+    }
